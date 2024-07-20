c1535b83cbf87ffd332eee0b91fcec0d579b7b79
Sora Harada <92529068+Saku052@users.noreply.github.com>
2024-07-20 21:39:17 +1000

    Initial Commit

---
+++ ReadingNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReadingNote
+//
+//  Created by Sora Harada on 20/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReadingNote/ReadingNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingNoteApp.swift
+//  ReadingNote
+//
+//  Created by Sora Harada on 20/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReadingNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
