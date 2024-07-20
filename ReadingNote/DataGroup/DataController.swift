6272692a1245c663b9162933747e504af5cc7b48
Sora Harada <92529068+Saku052@users.noreply.github.com>
2024-07-20 22:02:56 +1000

    Added DataController and Set initially

---
+++ ReadingNote/DataGroup/DataController.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    
+    let container = NSPersistentContainer(name: "FoodModel")
+    
+    init() {
+        container.loadPersistentStores { desc, error in
+            if let error = error {
+                print("Failed to load the data \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    
+}
